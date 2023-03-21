f6ebad70039558abdd3270641144a51d96ea78e1
lucifer2323 <42244729+lucifer2323@users.noreply.github.com>
2023-03-21 23:11:17 +0530

    Caption Generation controller
    Added Caption Generator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var router = express.Router();
+var {GenerateCaption} = require('../controllers/genCaption');
+
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("shajid playboy");
-});
+
+router.get('/',(req,res)=>{
+
+  res.status(200).json({message:"Ahzam Kaaham"})
+})
+
+router.post("/", GenerateCaption);
 
 module.exports = router;
