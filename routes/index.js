33c863c437613700ae86de5cc9f3cea1d03ed7b7
Fawmee Ahzam <93484273+dev001-silverlineit@users.noreply.github.com>
2023-03-22 17:17:37 +0530

    defe

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var router = express.Router();
-var { GenerateCaption } = require('../controllers/generateAI/genCaption');
+var { GenerateCaption } = require("../controllers/generateAI/genCaption");
 
 /* GET home page. */
 
-router.get('/', (req, res) => {
-
-  res.status(200).json({ message: "Ahzam Kaaham" })
-})
+router.get("/", (req, res) => {
+  res.status(200).json({ message: process.env.OPENAI_API_KEY });
+});
 
 router.post("/", GenerateCaption);
 
